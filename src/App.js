3342985f80e60df5f7c4f11588d8971a339b7f6e
UsamaHussain000111 <usamahussaincs@gmail.com>
2022-03-23 01:11:58 +0500

    Wildfires done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { useState , useEffect } from 'react'
 import Map from './components/Map'
+import Loader from './components/Loader'
+import Header from './components/Header'
 
 function App() {
+
+  const [ eventData , setEventData ] = useState([]);
+  const [ loading , setLoading ] = useState(false);
+
+  useEffect( () => {
+
+    const fetchEvents = async () => {
+
+      setLoading(true);
+
+      const response = await fetch('https://eonet.gsfc.nasa.gov/api/v2.1/events')
+      const { events } = await response.json()
+
+      setEventData(events)
+      setLoading(false)
+
+    }
+
+    fetchEvents()
+
+  } , [])
+
   return (
     <div>
-      <Map />
+      <Header />
+      { !loading ? <Map eventData={ eventData } /> : <Loader />}
     </div>
   );
 }
